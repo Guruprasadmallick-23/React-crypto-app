d018e2d976d6c3448e8272bba965807458b4a894
Guruprasadmallick-23 <guruprasadmallick333@gmail.com>
2022-02-24 19:56:44 +0530

    first commit

---
+++ src/globelstyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+   
+    margin:3px;
+    padding: 3px;
+    font-family: 'Kanit', sans-serif;
+   
+    
+  }
+`;
